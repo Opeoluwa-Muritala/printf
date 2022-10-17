d2a674bc13b1930f1f0e3f741450219f097f5b67
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-10-17 20:28:47 +0100

    all

--- main.h
+++ main.h
@@ -3,7 +3,6 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <libioP.h>
 
 void _putchar(char c);
 int _printf(const char *format, ...);
--- printf.c
+++ printf.c
@@ -2,19 +2,23 @@
 
 #undef printf
 
+/*
+ * _printf - Copy of printf function
+ *
+ * @format: format of text
+ *
+ * Return: Done error -1
+ */
+
 int _printf(const char *format, ...)
 {
 	va_list arg;
 	int done;
 
-	va_start (arg, format);
-	done = _putchar (stdout, format, arg);
-	va_end (arg);
+	va_start(arg, format);
+	done = vfprintf(stdout, format, arg);
+	va_end(arg);
 
-	return done;
+	return (done);
 }
-
-#undef _IO_printf
-ldbl_strong_alias (_printf,printf);
-ldbl_strong_alias (_printf,_IO_printf);
 
